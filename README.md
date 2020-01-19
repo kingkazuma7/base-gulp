2a5c2ba02387a0b4b39cf22b77a2a0fd54ae33f4
kingkazuma7 <nasionasio7@gmail.com>
2020-01-19 14:26:28 +0900

    [Update] アップデート

---
+++ README.md
@@ -0,0 +1,6 @@
+node_modules/css-declaration-sorter/src/index.js
+修正する
+
+// const { readFile } = require('fs').promises;
+↓
+const fs = require('fs').promises
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,97 @@
-// gulpプラグインの読み込み
-const gulp = require("gulp");
-// Sassをコンパイルするプラグインの読み込み
-const sass = require("gulp-sass");
-const isChanged = require("gulp-changed");
-const minifyImg = require("gulp-imagemin");
-const minifyImg_JPG = require("imagemin-jpeg-recompress");
-const minifyImg_PNG = require("imagemin-pngquant");
-const minifyImg_GIF = require("imagemin-gifsicle");
+var gulp = require('gulp');
+var sass = require('gulp-sass'); //Sassコンパイル
+var plumber = require('gulp-plumber'); //エラー時の強制終了を防止
+var notify = require('gulp-notify'); //エラー発生時にデスクトップ通知する
+var sassGlob = require('gulp-sass-glob'); //@importの記述を簡潔にする
+var browserSync = require( 'browser-sync' ); //ブラウザ反映
+var postcss = require('gulp-postcss'); //autoprefixerとセット
+var autoprefixer = require('autoprefixer'); //ベンダープレフィックス付与
+var cssdeclsort = require('css-declaration-sorter'); //css並べ替え
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var mozjpeg = require('imagemin-mozjpeg');
+var ejs = require("gulp-ejs");
+var rename = require("gulp-rename"); //.ejsの拡張子を変更
 
-
-// style.scssの監視タスクを作成する
-gulp.task("default", function() {
-  // ★ style.scssファイルを監視
-  return gulp.watch("./src/scss/style.scss", function() {
-    // style.scssの更新があった場合の処理
-
-    // style.scssファイルを取得
-    return (
-      gulp
-        .src("./src/scss/style.scss")
-        // Sassのコンパイルを実行
-        .pipe(
-          sass({
-            outputStyle: "expanded"
-          })
-            // sassエラー表示(これがないと自動的に止まってしまう)
-            .on("error", sass.logError)
-        )
-        // cssフォルダー以下に保存
-        .pipe(gulp.dest("./src/css"))
-    );
-  });
+// scssのコンパイル
+gulp.task('sass', function() {
+return gulp
+.src( './src/scss/**/*.scss' )
+.pipe( plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }) )//エラーチェック
+.pipe( sassGlob() )//importの読み込みを簡潔にする
+.pipe( sass({
+outputStyle: 'expanded' //expanded, nested, campact, compressedから選択
+}) )
+.pipe( postcss([ autoprefixer(
+{
+// ☆IEは11以上、Androidは4.4以上
+// その他は最新2バージョンで必要なベンダープレフィックスを付与する
+"overrideBrowserslist": ["last 2 versions", "ie >= 11", "Android >= 4"],
+cascade: false}
+) ]) )
+.pipe( postcss([ cssdeclsort({ order: 'alphabetically' }) ]) )//プロパティをソートし直す(アルファベット順)
+.pipe(gulp.dest('./src/css'));//コンパイル後の出力先
 });
 
-// 画像圧縮
-// https://memorandums.3ki3ki.com/gulp-image/
-gulp.task('minify-img', function(done) {
-  gulp.src("src/images/*.+(jpg|jpeg|png|gif)") //src部分は適宜環境に合わせて修正
-      .pipe(isChanged("./src/images/dist"))
-      .pipe(minifyImg([minifyImg_JPG(),minifyImg_PNG(),minifyImg_GIF()]))
-      .pipe(gulp.dest("./src/images/dist"));
-  done();
-});
-gulp.task('default', gulp.series('minify-img'));
-gulp.watch(['./src/*.*'], gulp.task('minify-img')); //src部分は適宜環境に合わせて修正
+// 保存時のリロード
+gulp.task( 'browser-sync', function(done) {
+browserSync.init({
 
+//ローカル開発
+server: {
+baseDir: "./",
+index: "index.html"
+}
+});
+done();
+});
+
+gulp.task( 'bs-reload', function(done) {
+browserSync.reload();
+done();
+});
+
+gulp.task("ejs", (done) => {
+gulp
+.src(["ejs/**/*.ejs", "!" + "ejs/**/_*.ejs"])
+.pipe( plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }) )//エラーチェック
+.pipe(rename({extname: ".html"})) //拡張子をhtmlに
+.pipe(gulp.dest("./")); //出力先
+done();
+});
+
+// 監視
+gulp.task( 'watch', function(done) {
+gulp.watch( './src/scss/**/*.scss', gulp.task('sass') ); //sassが更新されたらgulp sassを実行
+gulp.watch('./src/scss/**/*.scss', gulp.task('bs-reload')); //sassが更新されたらbs-reloadを実行
+gulp.watch( './src/js/*.js', gulp.task('bs-reload') ); //jsが更新されたらbs-relaodを実行
+gulp.watch('./ejs/**/*.ejs',gulp.task('ejs') ) ; //ejsが更新されたらgulp-ejsを実行
+gulp.watch('./ejs/**/*.ejs',gulp.task('bs-reload') ) ; //ejsが更新されたらbs-reloadを実行
+gulp.watch('./**/*.html',gulp.task('bs-reload') ) ; //ejsが更新されたらbs-reloadを実行
+});
+
+// default
+gulp.task('default', gulp.series(gulp.parallel('browser-sync', 'watch')));
+
+//圧縮率の定義
+var imageminOption = [
+pngquant({ quality: [70-85], }),
+mozjpeg({ quality: 85 }),
+imagemin.gifsicle({
+interlaced: false,
+optimizationLevel: 1,
+colors: 256
+}),
+// imagemin.jpegtran(),
+imagemin.optipng(),
+imagemin.svgo()
+];
+// 画像の圧縮
+// $ gulp imageminで./src/img/base/フォルダ内の画像を圧縮し./src/img/フォルダへ
+// .gifが入っているとエラーが出る
+gulp.task('imagemin', function () {
+return gulp
+.src('./src/img/base/*.{png,jpg,gif,svg}')
+.pipe(imagemin(imageminOption))
+.pipe(gulp.dest('./src/img'));
+});
